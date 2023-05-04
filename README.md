bdb2954ab9b3a267db6790d6851a01abe08da1c5
Ali Ashraf <89378903+webashraf@users.noreply.github.com>
2023-05-04 20:11:18 +0600

    add test readme.md file list item

---
+++ README.md
@@ -0,0 +1,9 @@
+Web Site Feature:
+* Item 1
+  * Subitem A
+  * Subitem B
+* Item 2
+  * Subitem C
+
+
+Live link : https://flavor-lav-a10.web.app/
--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -3,7 +3,7 @@ import "./Banner.css";
 
 const Banner = () => {
   return (
-    <div className=" headerBanner md:h-screen border bg-cover bg-top bg-fixed flex items-center justify-start z-0 md:px-12 px-6 py-20">
+    <div className=" headerBanner md:h-[95vh] border bg-cover bg-top bg-fixed flex items-center justify-start z-0 md:px-12 px-6 py-20">
       <div className=" z-10 uppercase border-b-8 rounded-b-2xl text-white w-1/2">
         <h1 className="text-3xl md:text-6xl md:leading-[3.9rem] font-semibold mb-1">
           Delight your{" "}
