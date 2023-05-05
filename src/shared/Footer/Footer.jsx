0e8e655120ea73e8797a2d350404860627f41487
Ali Ashraf <89378903+webashraf@users.noreply.github.com>
2023-05-05 16:41:25 +0600

    remove a list item in the footer

--- src/shared/Footer/Footer.jsx
+++ src/shared/Footer/Footer.jsx
@@ -17,7 +17,6 @@ const Footer = () => {
     <a className="link link-hover">About us</a> 
     <a className="link link-hover">Contact</a> 
     <a className="link link-hover">Jobs</a> 
-    <a className="link link-hover">Press kit</a>
   </div> 
   <div>
     <span className="footer-title">Legal</span> 
